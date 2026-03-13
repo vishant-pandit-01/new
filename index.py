76154e1d7a3f56bca872d2ff7104e2b4f1c27399
VISHANT PANDIT <vishantpandit80@gmail.com>
2026-03-13 09:56:53 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,1 @@
-class BankAccount:
-    
-    def __init__(self, name, balance=0):
-        self.name = name
-        self.balance = balance
-
-    def deposit(self, amount):
-        self.balance += amount
-        print(f"₹{amount} deposited success
+cl
