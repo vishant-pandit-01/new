0497c24376951b1c01241ead0199006d71588a30
VISHANT PANDIT <vishantpandit80@gmail.com>
2026-03-14 20:33:01 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,22 +1,28 @@
 class BankAccount:
+
     
     def __init__(self, name, balance=0):
         self.name = name
         self.balance = balance
 
+    
     def deposit(self, amount):
         self.balance += amount
         print(f"₹{amount} deposited successfully.")
 
+    
     def withdraw(self, amount):
         if amount > self.balance:
             print("Insufficient balance!")
+            
         else:
             self.balance -= amount
             print(f"₹{amount} withdrawn successfully.")
 
+    
     def check_balance(self):
         print(f"Current Balance: ₹{self.balance}")
+
 
 
 
