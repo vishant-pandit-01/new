2187ebe7af5cc123f169773a893cb79b472f78c9
VISHANT PANDIT <vishantpandit80@gmail.com>
2026-03-20 20:54:31 +0530

    Update index.py

--- index.py
+++ index.py
@@ -4,8 +4,10 @@ import time
 while True:
     now = datetime.datetime.now()
                  #for 24 hour format
+    
     # print(now.strftime("%H:%M:%S"), end="\r")       
 
                 #for 12 hour format
+    
     print(now.strftime("%I:%M:%S"), end="\r")        
     time.sleep(1)
