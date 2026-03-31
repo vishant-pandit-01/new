a4584b04ccc5d10011cebf7fc9d1b4e89888ae6a
VISHANT PANDIT <vishantpandit80@gmail.com>
2026-03-31 21:06:54 +0530

    Refactor time display loop in index.py
    Reformatted code for better readability and consistency.

--- index.py
+++ index.py
@@ -2,12 +2,8 @@ import datetime
 import time
 while True:
     now = datetime.datetime.now()
-
-    
-                 #for 24 hour format
+    #for 24 hour format
     # print(now.strftime("%H:%M:%S"), end="\r")       
-                #for 12 hour format
-
-    
+     #for 12 hour format
     print(now.strftime("%I:%M:%S"), end="\r")        
     time.sleep(1)
